41d191e9b174a7517e46104302bf76c4b1cadbff
Aleksey <vaznoe2024@mail.ru>
2025-11-17 17:49:39 +0300

    v.0 Создание проекта

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.graalvm.buildtools.native") version "0.10.6"
+}
+
+group = "ALL"
+version = "0.0.1-SNAPSHOT"
+description = "W_Project"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/all/w_project/WProjectApplication.java
@@ -0,0 +1,13 @@
+package all.w_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WProjectApplication.class, args);
+    }
+
+}
