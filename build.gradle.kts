90e82367067793d728f42fc41bdc59ebaa5fd051
Aleksey <vaznoe2024@mail.ru>
2025-11-19 01:34:30 +0300

    v.1 Подключение PostgreSQL с JPA/Hibernate

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql")
 }
 
 tasks.withType<Test> {
--- src/main/java/all/w_project/CalcModel.java
+++ src/main/java/all/w_project/CalcModel.java
@@ -1,4 +1,86 @@
 package all.w_project;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "calc_models")
 public class CalcModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String tag;
+    private BigDecimal principal;
+    private BigDecimal rate;
+    private int period;
+    private BigDecimal rezult;
+
+    CalcModel() {
+
+    }
+
+    CalcModel(String id, String tag, BigDecimal principal, BigDecimal rate, int period, BigDecimal rezult) {
+        this.id = id;
+        this.tag = tag;
+        this.principal = principal;
+        this.rate = rate;
+        this.period = period;
+        this.rezult = rezult;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public BigDecimal getPrincipal() {
+        return principal;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public BigDecimal getRezult() {
+        return rezult;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setPrincipal(BigDecimal principal) {
+        this.principal = principal;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    public void setRezult(BigDecimal rezult) {
+        this.rezult = rezult;
+    }
+
+    public String minToString() {
+        return "CalcModel [id=" + id + ", tag=" + tag + "]";
+    }
+    public String toString() {
+        return "CalcModel [id=" + id + ", tag=" + tag + ", principal=" + principal + ", rate=" + rate + ", period=" + period + ", rezult=" + rezult + "]";
+    }
 }
